5339ae84e7839c77ddb6afa2b79816c8626b2927
Luis Sanjuan <luis@risepal.com>
2023-05-12 10:42:47 -0500

    [bug need to open chrome for every user]

--- app.py
+++ app.py
@@ -9,14 +9,25 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 # Define the function to send a WhatsApp message using Selenium
-def send_whatsapp_message(phone_number, message):
-    # Initialize Chrome driver
+def send_whatsapp_message(df, message):
+
+    # Loop through each row and send the message to each user
+    for index, row in df.iterrows():
+        phone = row['phone'] 
+        send_message(phone, message)
+        st.success(f"Message sent to {phone}")
+
+        # Wait 5 seconds before sending the next message
+        time.sleep(5)
+
+
+# Define the function to send a WhatsApp message
+def send_message(phone, message):
+     # Initialize Chrome driver
     chromedriver_path = "/usr/local/bin/chromedriver"
     driver = webdriver.Chrome(chromedriver_path)
-
-
     # Compose the WhatsApp Web URL with the phone number and message
-    url = f"https://web.whatsapp.com/send?phone={phone_number}&text={message}"
+    url = f"https://web.whatsapp.com/send?phone={phone}&text={message}"
 
     # Load the WhatsApp Web URL
     driver.get(url)
@@ -24,7 +35,7 @@ def send_whatsapp_message(phone_number, message):
     # Wait for the chat window to load
     time.sleep(5)
 
-      # Find and click the "send" button
+    # Find and click the "send" button
     wait = WebDriverWait(driver, 30)  # Change the timeout value (30 seconds) as needed
     send_button = wait.until(EC.presence_of_element_located((By.XPATH, '//span[@data-icon="send"]')))
     send_button.click()
@@ -32,8 +43,7 @@ def send_whatsapp_message(phone_number, message):
     # Wait for the message to be sent
     time.sleep(10)
 
-    # Close the Chrome driver
-    driver.quit()
+# Define the Streamlit app
 
 # Define the Streamlit app
 def app():
@@ -41,13 +51,13 @@ def app():
 
     # Upload the CSV file
     csv_file = st.file_uploader("Upload CSV file", type=["csv"])
-    
     if csv_file is not None:
         # Read the CSV file into a pandas DataFrame
-        df = pd.read_csv(csv_file)
+        df = pd.read_csv(csv_file, dtype={"phone": str})
 
         # Display the DataFrame using Streamlit
         st.dataframe(df)
+  
     # Define the message to send
     message = st.text_input("Enter message to send")
 
@@ -55,16 +65,7 @@ def app():
     if st.button("Send"):
         # Read the CSV file
         if csv_file is not None:
-            df = pd.read_csv(csv_file)
-            st
-            # Loop through each row and send the message to each user
-            for index, row in df.iterrows():
-                phone = row['phone'] 
-                send_whatsapp_message(phone, message)
-                st.success(f"Message sent to {phone}")
-
-                # Wait 5 seconds before sending the next message
-                time.sleep(5)
+            send_whatsapp_message(df, message)
         else:
             st.error("Please upload a CSV file.")
 
